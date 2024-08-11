8df89bee0f47b91ede8b8ddde8c95fa273fcf772
Nikhita <nikhitareddyer@gmail.com>
2024-08-12 00:48:58 +0530

    New Updated Code

--- src/main/java/com/qa/pages/Home.java
+++ src/main/java/com/qa/pages/Home.java
@@ -1,5 +1,10 @@
 package com.qa.pages;
 
 public class Home {
+	
+	public void myHome() {
+		System.out.println("My Home Page");
+		System.out.println("Demo");
+	}
 
 }
---
+++ src/main/java/com/qa/pages/MyNewPage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class MyNewPage {
+	
+	public void newpage() {
+		System.out.println("My New Page");
+	}
+
+}
